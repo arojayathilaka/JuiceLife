0e0484a1c5d24977b2e6fa4b2e8e58ecdd5ae10f
arojayathilaka <acjayathilaka@gmail.com>
2019-08-07 16:53:40 +0530

    1st commit

---
+++ app/src/main/java/com/example/juicelife/delCity.java
@@ -0,0 +1,31 @@
+package com.example.juicelife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class delCity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_del_city);
+
+        Spinner spinner = (Spinner) findViewById(R.id.selectCity);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(delCity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.cities));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(myAdapter);
+    }
+
+    public void onClickSaveDel(View view){
+        startActivity(new Intent(delCity.this, ConfirmDelivery.class ));
+    }
+
+
+}
